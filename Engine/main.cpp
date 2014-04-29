9ad2968c1765bf2d538bb0a4d33fecc293dd3e3d
Mats <matsallen@me.com>
2014-04-29 14:41:33 -0700

    Initial Commit

---
+++ Engine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Engine
+//
+//  Created by Mats Allen on 4/29/14.
+//  Copyright (c) 2014 MHA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
